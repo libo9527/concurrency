f7220cd14129b963de098cf14b86eb650ac236f4
libo <18629374709@163.com>
2018-11-06 20:14:20 +0800

    add annotations

---
+++ concurrency/src/main/java/com/mmall/concurrency/annotations/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Description： 课程里用来标记【不推荐】的类或者写法
+ * @Auther： libo
+ * @date： 2018/11/6:19:32
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
---
+++ concurrency/src/main/java/com/mmall/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Description： 课程里用来标记【线程安全】的类或者写法
+ * @Auther： libo
+ * @date： 2018/11/6:19:32
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
