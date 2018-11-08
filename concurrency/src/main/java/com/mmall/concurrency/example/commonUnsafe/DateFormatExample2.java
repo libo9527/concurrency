22c636c277b1aca23ade7338d4be04084aa60f7f
libo <18629374709@163.com>
2018-11-08 09:51:14 +0800

    DateFormat and JodaTime

---
+++ concurrency/src/main/java/com/mmall/concurrency/example/commonUnsafe/DateFormatExample2.java
@@ -0,0 +1,56 @@
+package com.mmall.concurrency.example.commonUnsafe;
+
+import com.mmall.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Description：
+ * @Auther： libo
+ * @date： 2018/11/8:09:31
+ */
+@Slf4j
+@ThreadSafe
+public class DateFormatExample2 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    public static void update() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        try {
+            simpleDateFormat.parse("20181108");
+        } catch (ParseException e) {
+            log.error("parse Exception", e);
+        }
+    }
+}
