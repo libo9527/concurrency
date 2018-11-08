99346ad5ffb4007298c7b01761e4506bc81a0ddf
libo <18629374709@163.com>
2018-11-08 22:50:03 +0800

    AQS-Semaphore.

---
+++ concurrency/src/main/java/com/mmall/concurrency/example/aqs/SemaphoreExample2.java
@@ -0,0 +1,45 @@
+package com.mmall.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Description：
+ * @Auther： libo
+ * @date： 2018/11/8:22:19
+ */
+@Slf4j
+public class SemaphoreExample2 {
+
+    private final static int threadCount = 20;
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire(3); // 获取多个许可
+                    test(threadNum);
+                    semaphore.release(3); // 释放多个许可
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+            });
+        }
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        Thread.sleep(1000);
+        log.info("{}", threadNum);
+    }
+}
